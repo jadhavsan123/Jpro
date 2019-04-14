723283d0cb6fac756f319db4fec1b1d0addaef9f
jadhavsan123 <jadhav.sandhya843@gmail.com>
2019-04-14 18:39:15 +0530

    First Java Code Push

---
+++ JavaDemo/src/FirstProgram/Welcome.java
@@ -0,0 +1,13 @@
+package FirstProgram;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome in java programming with eclip");
+		 int a=10;
+		 int b=89;
+		 int result=a+b;
+		 System.out.println("result="+result);
+	}
+
+}
